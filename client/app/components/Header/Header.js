7257cbdfb7d654481c977e4e5dedd91f822ee25b
swapnilkothawade <swapnil7080@gmail.com>
2019-01-15 17:07:43 -0500

    Changes for graph

--- client/app/components/Header/Header.js
+++ client/app/components/Header/Header.js
@@ -4,9 +4,9 @@ const Header = props => {
   const rate = props.currentRates && props.currentRates.rates && props.currentRates.rates["CAD"];
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <span className="navbar-brand text-bold">
-        Last updated at <b>{props.updatedDate}</b> Current <b>USD/CAD</b> rate is <b>{rate}</b>
-      </span>
+      <div className="navbar-brand text-bold">
+        <div className="pull-left">Last updated at <b>{props.updatedDate}</b></div><div className="pull-right">Current <b>USD/CAD</b> rate is <b>{rate}</b></div>
+      </div>
     </nav>
   );
 };
